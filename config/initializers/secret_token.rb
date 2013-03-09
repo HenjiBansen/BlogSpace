264f39d6418bee9477dbf92b070065f630ea902a
henjibansen <benjamin@platfor.ms>
2013-03-09 17:46:02 -0600

    Fist commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogSpace::Application.config.secret_token = '50d55537a9a57d62c279ac261cbd44ab0abe93b7a2ef170729afa414445707af0f64ecdd1e9ffbdac2c26565ea7904ba5bdd761d683d80b6b285b3c0cd461faa'
